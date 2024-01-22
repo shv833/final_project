8350101764182b1bb333176248b39a5af29e4213
Volodymyr Shyra <87500387+shv833@users.noreply.github.com>
2024-01-22 23:52:23 +0200

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,79 @@
+from AddressBook import *
+from abc import ABC, abstractmethod
+
+
+class AbstractBot(ABC):
+    def __init__(self):
+        self.book = AddressBook()
+
+    @abstractmethod
+    def handle(self):
+        pass
+
+
+class AddBot(AbstractBot):
+    def handle(self):
+        name = Name(input("Name: ")).value.strip()
+        phones = Phone().value
+        birth = Birthday().value
+        email = Email().value.strip()
+        status = Status().value.strip()
+        note = Note(input("Note: ")).value
+        record = Record(name, phones, birth, email, status, note)
+        return self.book.add(record)
+
+
+class SearchBot(AbstractBot):
+    def handle(self):
+        print("There are following categories: \nName \nPhones \nBirthday \nEmail \nStatus \nNote")
+        category = input('Search category: ')
+        pattern = input('Search pattern: ')
+        result = (self.book.search(pattern, category))
+        for account in result:
+            if account['birthday']:
+                birth = account['birthday'].strftime("%d/%m/%Y")
+                result = "_" * 50 + "\n" + f"Name: {account['name']} \nPhones: {', '.join(account['phones'])} \nBirthday: {birth} \nEmail: {account['email']} \nStatus: {account['status']} \nNote: {account['note']}\n" + "_" * 50
+                print(result)
+
+
+class EditBot(AbstractBot):
+    def handle(self):
+        contact_name = input('Contact name: ')
+        parameter = input('Which parameter to edit(name, phones, birthday, status, email, note): ').strip()
+        new_value = input("New Value: ")
+        return self.book.edit(contact_name, parameter, new_value)
+
+
+class RemoveBot(AbstractBot):
+    def handle(self):
+        pattern = input("Remove (contact name or phone): ")
+        return self.book.remove(pattern)
+
+
+class SaveBot(AbstractBot):
+    def handle(self):
+        file_name = input("File name: ")
+        return self.book.save(file_name)
+
+
+class LoadBot(AbstractBot):
+    def handle(self):
+        file_name = input("File name: ")
+        return self.book.load(file_name)
+
+
+class CongratulateBot(AbstractBot):
+    def handle(self):
+        print(self.book.congratulate())
+
+
+class ViewBot(AbstractBot):
+    def handle(self):
+        print(self.book)
+
+
+class ExitBot(AbstractBot):
+    def handle(self):
+        print('Bye')
+        exit()
+
---
+++ __main__.py
@@ -0,0 +1,34 @@
+from Bot import *
+
+
+if __name__ == "__main__":
+    print('Hello. I am your contact-assistant. What should I do with your contacts?')
+    
+    choice = {
+        'add': AddBot(),
+        'search': SearchBot(),
+        'edit': EditBot(),
+        'remove': RemoveBot(),
+        'save': SaveBot(),
+        'load': LoadBot(),
+        'congratulate': CongratulateBot(),
+        'view': ViewBot(),
+        'exit': ExitBot()
+    }
+
+
+    while True:
+        action = input('Type help for list of commands or enter your command\n').strip().lower()
+
+        if action == 'help':
+            format_str = str('{:%s%d}' % ('^',20))
+            for command in choice:
+                print(format_str.format(command))
+
+        if action in choice:
+            choice[action].handle()
+            if action in ['add', 'remove', 'edit']:
+                choice[action].book.save("auto_save")
+        else:
+            print("Incorrect option")
+
